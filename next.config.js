70df22494892ad0e5ffe9d298e12eb8942ebb7e3
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2024-05-06 13:08:39 +0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  nextConfig,
+  images: {
+    loader: 'akamai',
+    path: '',
+    domains: ['github.com'],
+  }
+}
